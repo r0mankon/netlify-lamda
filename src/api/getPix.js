a8935a0f4eed6ea84d6d4163fd936c859574837c
rokonxr <rokon.rxr@gmail.com>
2020-12-16 02:09:25 +0600

    add types

--- src/api/getPix.js
+++ src/api/getPix.js
@@ -1,6 +1,11 @@
+import { APIGatewayEvent, Context } from "aws-lambda";
 import fetch from "node-fetch";
 import encoding from "encoding";
 
+/**
+ * @param { APIGatewayEvent } event
+ * @param { Context } ctx
+ */
 export async function handler(event, ctx) {
   const res = await fetch("https://pixabay.com/api/?key=" + process.env.API_KEY);
   const data = await res.json();
